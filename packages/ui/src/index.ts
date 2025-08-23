65c048f60966ff21bc89583339bbd5812aa46b12
kwang-arcfusion <punyarit.tha@arcfusion.ai>
2025-08-23 23:55:58 +0700

    fix components structure

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,9 +1,7 @@
 export * from './hooks/useGlobalStyles';
 
-export { Button } from './Button';
-export type { ButtonProps } from './Button'; // Export ตัว Type ของ Props ออกไปด้วย
-export * from './ThemeToggle';
-export * from './Sidebar';
-export * from './Topbar';
-export * from './AppShell';
-export type { AppShellProps } from './AppShell';
+export * from './components/ThemeToggle';
+export * from './components/Sidebar';
+export * from './components/Topbar';
+export * from './components/AppShell';
+export type { AppShellProps } from './components/AppShell';
