cfcf17e49389db5b2af8b0fd95276000ff39d0fe
kwang-arcfusion <punyarit.tha@arcfusion.ai>
2025-08-24 17:25:08 +0700

    remove typescript for every project. use typescript root instead

--- hosts/knowesis/src/hooks/useTopbar.ts
+++ hosts/knowesis/src/hooks/useTopbar.ts
@@ -1,11 +1,4 @@
 // hosts/knowesis/src/hooks/useTopbar.ts
 import { useTopbarStore } from '../stores/topbarStore';
 
-// Export store hook ออกไปภายใต้ชื่อ useTopbar เพื่อให้เรียกใช้งานได้ง่าย
-
-
-
-
-
-
 export const useTopbar = useTopbarStore;
